da1d63616bd2433f24d7ffabc05ae348ec64f41f
Erik Melton <mudmander@gmail.com>
2016-07-21 17:25:26 -0700

    Dashboard set up w/redirection

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
   def index
+    if(!client_signed_in?)
+      redirect_to '/'
+    end
+
     @person = Person.first
     flash[:success] = "Welcome to the Dashboard!"
     end
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,8 @@
 class HomepageController < ApplicationController
   def index
+    if(client_signed_in?)
+      redirect_to '/dashboard/index'
+    end
   end
 
   def signin
