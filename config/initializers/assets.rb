61df1398a538ac4d47412ed44afad72d69aff19f
Erik Melton <mudmander@gmail.com>
2016-07-21 17:46:38 -0700

    Added editable grid

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,11 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( editablegrid.css )
+Rails.application.config.assets.precompile += %w( editablegrid.js )
+Rails.application.config.assets.precompile += %w( editablegrid_charts.js )
+Rails.application.config.assets.precompile += %w( editablegrid_charts_ofc.js )
+Rails.application.config.assets.precompile += %w( editablegrid_editors.js )
+Rails.application.config.assets.precompile += %w( editablegrid_renderers.js )
+Rails.application.config.assets.precompile += %w( editablegrid_utils.js )
+Rails.application.config.assets.precompile += %w( editablegrid_validators.js )
