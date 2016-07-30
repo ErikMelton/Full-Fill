a9c7b662cdb86c7b246b4ec8b5af61cd13bd8445
Erik Melton <mudmander@gmail.com>
2016-07-30 16:18:54 -0700

    Added various things to the lists

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -26,7 +26,60 @@ class DashboardController < ApplicationController
     @numOfActivityColumns = 4
 
     calc_age_range
+    input_activities
     calc_scores
+
+  end
+
+  def input_activities
+    @activities = Activity.all()
+
+    @physString = ''
+    @expressString = ''
+    @creativeString = ''
+    @abstractString = ''
+    @socialString = ''
+
+    @activities.each do |activity|
+      trait_spec = Trait.where(trait_spec_id: activity.trait_id).first
+      facet_spec = Facet.where(id: trait_spec.facet_id).first
+
+      if(facet_spec.facettype == "physical")
+        key = activity.activityname
+        name = activity.activityname.split.map(&:capitalize).join(' ')
+        @physString << '"' + "#{key}" + '"' + ':' + '"' + "#{name}" + '"' + ','
+      end
+      if(facet_spec.facettype == "expressive")
+        key = activity.activityname
+        name = activity.activityname.split.map(&:capitalize).join(' ')
+        @expressString << '"' + "#{key}" + '"' + ':' + '"' + "#{name}" + '"' + ','
+      end
+      if(facet_spec.facettype == "creative")
+        key = activity.activityname
+        name = activity.activityname.split.map(&:capitalize).join(' ')
+        @creativeString << '"' + "#{key}" + '"' + ':' + '"' + "#{name}" + '"' + ','
+      end
+      if(facet_spec.facettype == "abstract")
+        key = activity.activityname
+        name = activity.activityname.split.map(&:capitalize).join(' ')
+        @abstractString << '"' + "#{key}" + '"' + ':' + '"' + "#{name}" + '"' + ','
+      end
+      if(facet_spec.facettype == "social")
+        key = activity.activityname
+        name = activity.activityname.split.map(&:capitalize).join(' ')
+        @socialString << '"' + "#{key}" + '"' + ':' + '"' + "#{name}" + '"' + ','
+      end
+    end
+    @physString = @physString[0...-1] + '}'
+    @physString = @physString.insert(0, "{");
+    @expressString = @expressString[0...-1] + '}'
+    @expressString = @expressString.insert(0, "{");
+    @creativeString = @creativeString[0...-1] + '}'
+    @creativeString = @creativeString.insert(0, "{");
+    @abstractString = @abstractString[0...-1] + '}'
+    @abstractString = @abstractString.insert(0, "{");
+    @socialString = @socialString[0...-1] + '}'
+    @socialString = @socialString.insert(0, "{");
   end
 
   def calc_age_range
